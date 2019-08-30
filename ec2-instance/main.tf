ded798e72bffa64bf3637f293ce2b08dcaefdd7c
Ryan <ryan.fee625@gmail.com>
2019-08-30 13:09:08 -0400

    combined into folder

---
+++ ec2-instance/main.tf
@@ -0,0 +1,15 @@
+
+provider "aws" {
+  access_key = var.scalr_aws_access_key
+  secret_key = var.scalr_aws_secret_key
+  region     = var.region
+}
+
+resource "aws_instance" "scalr" {
+  count = 1
+  ami                    = "ami-2757f631"
+  instance_type          = "t2.nano"
+  subnet_id              = "subnet-0ebb1058ad727cfdb"
+  vpc_security_group_ids = ["sg-0880cfdc546b123ba"]
+  key_name               = "ryan"
+}
---
+++ ec2-instance/scalr-module.hcl
@@ -0,0 +1,12 @@
+version = "v1"
+
+variable "billing_code" {
+  global_variable = "billing_code"
+}
+
+#variable "region" {
+#  policy = "cloud.locations"
+#  conditions = {
+#  cloud = "ec2"
+#  }
+#}
