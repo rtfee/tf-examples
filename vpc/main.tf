edfa2f6f097fdf4db85265c362c7d986361ecc3f
Ryan <ryan.fee625@gmail.com>
2019-09-03 10:32:44 -0400

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -4,7 +4,6 @@ provider "aws" {
   region     = var.region
 }
 
-<<<<<<< HEAD
 module "vpc" "scalr" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -12,10 +11,6 @@ module "vpc" "scalr" {
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   public_subnets  = ["10.2.0.0/28", "10.2.0.16/28", "10.2.0.32/28"]
-=======
-resource "aws_vpc" "scalr" {
-  cidr_block = var.cidr
->>>>>>> 4fd23c12d87947509814ff7799601f9b897cc7f0
 
   tags = {
     Name = var.name
