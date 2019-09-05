ee9607f7899df8f64f32f43e29b588a183a79554
Ryan <ryan.fee625@gmail.com>
2019-09-05 09:53:32 -0400

    Update instances.tf

--- elb_w_instances/instances.tf
+++ elb_w_instances/instances.tf
@@ -7,15 +7,15 @@ provider "aws" {
 resource "aws_instance" "scalr" {
   count = 2
   ami                    = "ami-2757f631"
-  instance_type          = "t2.micro"
+  instance_type          = "t2.nano"
   subnet_id              = "subnet-0ebb1058ad727cfdb"
   vpc_security_group_ids = ["sg-02228c3d8e04c8951"]
   key_name               = "ryan"
-#  provisioner "local-exec" {
-#  command = <<EOH
-#     sudo apt-get install -y apache2
-# EOH
-#}
+  provisioner "local-exec" {
+    command = <<EOH
+     sudo apt-get install -y apache2
+ EOH
+}
 }
 
 output "instance_public_ips" {
