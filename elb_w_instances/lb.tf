4332c327381f2f4d0bc283305b496a032e00ce3b
Ryan <ryan.fee625@gmail.com>
2019-09-11 10:01:11 -0400

    Update lb.tf

--- elb_w_instances/lb.tf
+++ elb_w_instances/lb.tf
@@ -5,16 +5,16 @@ resource "aws_elb" "scalr" {
 
   listener {
     instance_port     = 80
-    instance_protocol = "http"
+    instance_protocol = "tcp"
     lb_port           = 80
-    lb_protocol       = "http"
+    lb_protocol       = "tcp"
   }
 
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:80/"
+    target              = "TCP:80"
     interval            = 30
   }
 
