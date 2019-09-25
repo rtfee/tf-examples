f036b3be02e1a4e50ef5eba4c2512cb0dae48fbe
Ryan <ryan.fee625@gmail.com>
2019-09-25 14:15:00 -0400

    Update main.tf

--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -1,7 +1,7 @@
 
 provider "aws" {
-  access_key = var.scalr_aws_access_key
-  secret_key = var.scalr_aws_secret_key
+  access_key = "${var.scalr_aws_access_key}"
+  secret_key = "${var.scalr_aws_secret_key}"
   region     = var.region
 }
 
