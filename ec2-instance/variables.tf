84be5cc39f6444ec8d497ef20de73c948feb90f1
Ryan <ryan.fee625@gmail.com>
2019-09-27 08:31:36 -0400

    Update variables.tf

--- ec2-instance/variables.tf
+++ ec2-instance/variables.tf
@@ -5,3 +5,17 @@ variable "region" {
 variable "scalr_aws_secret_key" {}
 
 variable "scalr_aws_access_key" {}
+
+variable "ami" {
+  default = "ami-2757f631"
+}
+
+variable "instance_type" {
+  default = "t2.nano"
+}
+
+variable "subnet" {}
+
+variable "sg" {}
+
+variable "key" {}
