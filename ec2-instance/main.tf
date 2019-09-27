0e9c54ec89779cf045c4937f5dbf3dcba75f01d4
Ryan <ryan.fee625@gmail.com>
2019-09-27 09:07:57 -0400

    Update main.tf

--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -9,6 +9,6 @@ resource "aws_instance" "scalr" {
   ami                    = var.ami
   instance_type          = var.instance_type
   subnet_id              = var.subnet
-  vpc_security_group_ids = ["sg-0880cfdc546b123ba"]
+  vpc_security_group_ids = [var.sg]
   key_name               = var.key
 }
