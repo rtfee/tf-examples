695c412f187f5681ea18ffa55da5c767ee4fc03d
Ryan <ryan.fee625@gmail.com>
2019-10-22 15:30:09 -0400

    Added ELB

--- elb/variables.tf
+++ elb/variables.tf
@@ -1,10 +1,6 @@
-variable "instance-id" {
-  type = "string"
-}
 
 variable "region" {
-  type = "string"
 }
 
 variable "scalr_aws_secret_key" {}
-variable "scalr_aws_access_key" {} 
+variable "scalr_aws_access_key" {}
