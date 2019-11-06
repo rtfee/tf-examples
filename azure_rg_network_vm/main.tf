8bf5e83dc3bb4b5248453d6000ecb1a332939812
Ryan <ryan.fee625@gmail.com>
2019-11-06 13:22:55 -0500

    Add Azure VM

--- azure_rg_network_vm/main.tf
+++ azure_rg_network_vm/main.tf
@@ -1,4 +1,5 @@
-# Configure the Azure Provider
+# refer to a resource group
+
 provider "azurerm" {
     subscription_id = var.scalr_azurerm_subscription_id
     client_id = var.scalr_azurerm_client_id
@@ -6,46 +7,53 @@ provider "azurerm" {
     tenant_id = var.scalr_azurerm_tenant_id
 }
 
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "test" {
-  name                = var.network_name
-  resource_group_name = var.rg_name
-  location            = var.region
-  address_space       = ["10.0.0.0/24"]
+data "azurerm_resource_group" "test" {
+  name = "scalr-demo"
 }
 
-resource "azurerm_subnet" "test" {
+#refer to a subnet
+data "azurerm_subnet" "test" {
   name                 = "test"
-  resource_group_name  = var.rg_name
-  virtual_network_name = "${azurerm_virtual_network.test.name}"
-  address_prefix       = "10.0.1.0/24"
+  virtual_network_name = "demo1"
+  resource_group_name  = "scalr-demo"
 }
 
+# Create public IPs
+resource "azurerm_public_ip" "test" {
+    name                         = "myPublicIP-test"
+    location                     = "${data.azurerm_resource_group.test.location}"
+    resource_group_name          = "${data.azurerm_resource_group.test.name}"
+    public_ip_address_allocation = "dynamic"
+
+}
+
+# create a network interface
 resource "azurerm_network_interface" "test" {
-  name                = var.net_interface_name
-  location            = var.region
-  resource_group_name = var.rg_name
+  name                = "nic-test"
+  location            = "${data.azurerm_resource_group.test.location}"
+  resource_group_name = "${data.azurerm_resource_group.test.name}"
 
   ip_configuration {
     name                          = "testconfiguration1"
-    subnet_id                     = "${azurerm_subnet.test.id}"
-    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = "${data.azurerm_subnet.test.id}"
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.test.id}"
   }
 }
 
+# Create virtual machine
 resource "azurerm_virtual_machine" "test" {
-  name                  = var.vm_name
-  location              = var.region
-  resource_group_name   = var.rg_name
-  network_interface_ids = ["${azurerm_network_interface.test.id}"]
-  vm_size               = var.instance_type
+    name                  = "myVM-test"
+    location              = "${azurerm_network_interface.test.location}"
+    resource_group_name   = "${data.azurerm_resource_group.test.name}"
+    network_interface_ids = ["${azurerm_network_interface.test.id}"]
+    vm_size               = "Standard_DS1_v2"
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
+# Uncomment this line to delete the OS disk automatically when deleting the VM
+delete_os_disk_on_termination = true
 
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+# Uncomment this line to delete the data disks automatically when deleting the VM
+delete_data_disks_on_termination = true
 
   storage_image_reference {
     publisher = "Canonical"
@@ -53,21 +61,19 @@ resource "azurerm_virtual_machine" "test" {
     sku       = "16.04-LTS"
     version   = "latest"
   }
-  storage_os_disk {
+   storage_os_disk {
     name              = "myosdisk1"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
-  os_profile {
+   os_profile {
     computer_name  = "hostname"
     admin_username = "testadmin"
     admin_password = "Password1234!"
   }
-  os_profile_linux_config {
+   os_profile_linux_config {
     disable_password_authentication = false
   }
-  tags = {
-    environment = "staging"
-  }
+
 }
