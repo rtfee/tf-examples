b08e040c6bcac7af270d82e8d94c29ce3f921c98
Ryan <ryan.fee625@gmail.com>
2019-12-03 14:44:11 -0500

    Create variables.tf

---
+++ eks_wordpress/variables.tf
@@ -0,0 +1,11 @@
+variable "scalr_aws_secret_key" {}
+variable "scalr_aws_access_key" {}
+variable "aws_region" {}
+variable "vpc_name" {}
+variable "eks_cluster_name" {}
+variable "kubernetes_version" {}
+variable "configure_kubectl" {}
+variable "kubectl_config_path" {}
+variable "configure_openid_connect_provider" {}
+variable "endpoint_public_access" {}
+variable "eks_worker_keypair_name" {}
