4f6e6706945ddd8eb6287d26815a7cafdef9dc6b
Ryan <ryan.fee625@gmail.com>
2019-12-09 15:41:05 -0500

    Update variables.tf

--- ec2_ansible/variables.tf
+++ ec2_ansible/variables.tf
@@ -38,4 +38,5 @@ variable "remote_host" {
 variable "private_key" {
   description = "The text of SSH Private key. This will be formatted by the Terraform template.<br>This will be used in the remote workspace to allow Terraform to connect to the servers and run scripts to configure Scalr. It only exists in the workspace for the duration of the run."
   type = string
+  default = "FROM_FILE"
 }
