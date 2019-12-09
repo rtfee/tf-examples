b9938195720fea6027fcef9e373bbe395f7c498e
Ryan <ryan.fee625@gmail.com>
2019-12-09 15:14:48 -0500

    Update main.tf

--- ec2_ansible/main.tf
+++ ec2_ansible/main.tf
@@ -1,15 +1,21 @@
 provider "aws" {
-  access_key = var.scalr_aws_access_key
-  secret_key = var.scalr_aws_secret_key
-  region     = "us-east-1"
+  access_key = "${var.scalr_aws_access_key}"
+  secret_key = "${var.scalr_aws_secret_key}"
+  region     = var.region
+}
+
+data "null_data_source" "values" {
+  inputs = {
+    vpc_id = var.vpc_id
+    }
 }
 
 resource "aws_instance" "scalr" {
   ami                    = var.ami
-  instance_type          = var.instancetype
+  instance_type          = var.instance_type
   subnet_id              = var.subnet
   vpc_security_group_ids = var.sg
-  key_name               = "ryan"
+  key_name               = var.key
 
   connection {
         host	= var.remote_host
