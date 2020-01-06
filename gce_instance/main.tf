2d92f74330cba2974054ece6f4861033f0f1c0a9
Ryan <ryan.fee625@gmail.com>
2020-01-06 16:12:26 -0500

    Update main.tf

--- gce_instance/main.tf
+++ gce_instance/main.tf
@@ -1,3 +1,9 @@
+  
+provider "google" {
+    project = var.scalr_google_project
+    credentials = var.scalr_google_credentials
+)
+
 resource "google_compute_instance" "default" {
   name         = "test"
   machine_type = "n1-standard-1"
