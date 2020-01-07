407708a16ebc6734f79a9602c12923a8920cffe5
Ryan <ryan.fee625@gmail.com>
2020-01-07 08:37:02 -0500

    Update variables.tf

--- gce_instance/variables.tf
+++ gce_instance/variables.tf
@@ -1,6 +1,10 @@
 variable "scalr_google_project" {}
 variable "scalr_google_credentials" {}
 variable "region" {}
-variable "zone" {}
-variable "instane_name" {}
+variable "zone" {
+  default = "us-east4-a"
+}
+variable "instane_name" {
+description = "Instance Name"  
+}
 variable "instance_type" {}
