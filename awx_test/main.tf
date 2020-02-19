4fa5792ba3eed911508b5a087945dda3675b47c8
Ryan <ryan.fee625@gmail.com>
2020-02-19 11:24:43 -0500

    Update main.tf

--- awx_test/main.tf
+++ awx_test/main.tf
@@ -4,7 +4,13 @@ provider "awx" {
 #  endpoint = var.endpoint
 }
 
-resource "awx_inventory" "terraform" {
-  name = "terraform"
-  organization_id = 2
+#resource "awx_inventory" "terraform" {
+#  name = "terraform"
+#  organization_id = 2
+#}
+
+resource "awx_team" "automation_team" {
+        name = "automation_team"
+        description = "Automation team"
+        organization_id = 1
 }
