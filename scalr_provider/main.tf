7cf5934bbab18b4a0fd28f61249fd9d2005a6e8c
Ryan <ryan.fee625@gmail.com>
2020-03-31 11:32:24 -0400

    Create main.tf

---
+++ scalr_provider/main.tf
@@ -0,0 +1,15 @@
+provider "scalr" {
+  hostname = var.scalr_server
+  token    = var.token
+}
+
+resource "scalr_workspace" "test" {
+  name         = var.workspace_name
+  organization = var.org
+  auto_apply = "true"
+  terraform_version = "0.12.19"
+  vcs_repo {
+     identifier = "scalr-eap/ec2_instance"
+     oauth_token_id = var.vcs_id
+    }
+}
