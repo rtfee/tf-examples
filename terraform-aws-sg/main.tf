3419411b87ca2afc53602da6eff17f0335a8b739
Ryan <ryan.fee625@gmail.com>
2022-03-01 08:38:16 -0500

    Create main.tf

---
+++ terraform-aws-sg/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = var.region
+}
+
+resource "aws_instance" "scalr" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet
+  vpc_security_group_ids = var.sg
+  key_name               = var.key
+  count                  = var.instance_count
+}
