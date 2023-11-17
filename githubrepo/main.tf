dfe15d277849a2d43ccb1a8b0456dc265eea1ac2
Ryan <ryan.fee625@gmail.com>
2023-11-17 14:51:33 -0500

    Update main.tf

--- githubrepo/main.tf
+++ githubrepo/main.tf
@@ -3,6 +3,18 @@
 #  organization = var.github_organization
 #}
 
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {}
+
 resource "github_repository" "example" {
   name        = var.repo_name
   description = "My awesome codebase"
