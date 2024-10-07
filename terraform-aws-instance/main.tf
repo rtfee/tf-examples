12c1951f62d7a44829fec909c02b3e0b3f3e7a36
Ryan <6885827+rtfee@users.noreply.github.com>
2024-10-07 14:41:47 -0400

    Update main.tf

--- terraform-aws-instance/main.tf
+++ terraform-aws-instance/main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "scalr" {
   ami                    = "ami-0c7217cdde317cfec"
   instance_type          = "t2.micro"
   subnet_id              = "subnet-0ebb1058ad727cfdb"
-  vpc_security_group_ids = "sgr-089e9aa5630ddad0c"
+  vpc_security_group_ids = ["sgr-089e9aa5630ddad0c"]
   key_name               = "ryan"
 }
